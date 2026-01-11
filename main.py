c6f3116edd05b09eaccaeddfb8eb064c90605876
Xie-qwp1234 <109st955160@gmail.com>
2026-01-11 15:09:33 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# 啟動迷你網頁，讓 Render 持續上線
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
